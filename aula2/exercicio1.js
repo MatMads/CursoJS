c9b5374339b1c22e156943281f3ffed81ec7ee36
Mateus Miranda <144238377+MatMads@users.noreply.github.com>
2024-06-01 10:33:10 -0300

    att
    Adição das aulas 2 e 3.

---
+++ aula2/exercicio1.js
@@ -0,0 +1,3 @@
+console.log(`Meu nome é "Mateus", 
+            eu estou aprendendo Javascript 
+            as`, 10, `horas da manhã`);
---
+++ aula3/comentarios.js
@@ -0,0 +1,14 @@
+//Comentários são ignorados pela IDE.
+
+console.log('linha 1'); //comentário
+console.log('linha 2'); //comentário
+console.log('linha 3'); //comentário
+console.log('linha 4'); //comentário
+
+//comentário de uma linha
+
+/* comentario em blocos
+    pode se expandir para
+    várias linhas. */
+
+
